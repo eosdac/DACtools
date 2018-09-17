9ccba9911d8ee97670788e8eb6af21c1d886b6cd
kasperfish <kasperkasperfish@gmail.com>
2018-09-17 20:46:36 +0200

    fix bug

--- profile_watcher/api/routes/routes.js
+++ profile_watcher/api/routes/routes.js
@@ -5,7 +5,7 @@ var appRouter = function (app, db) {
     app.get("/profile/:account", async function (req, res) {
 
         let account = req.params.account;
-        if (account.length >= 3 && account.length <= CONF.routes['/profile/:account'].max) {
+        if (account.length >= 3 && account.length <= CONF.api.routes['/profile/:account'].max) {
             db.collection('test').find({_id: account}).toArray((err, result) => {
                 if (err) return console.log(err);
 
@@ -17,9 +17,10 @@ var appRouter = function (app, db) {
             res.status(400).send({ message: 'invalid account supplied' });
         }
     });
+    
     app.post("/profiles", async function (req, res) {
       let accounts = req.body
-      if (accounts.length >= 1 && accounts.length <= CONF.routes['/profiles'].max) {
+      if (accounts.length >= 1 && accounts.length <= CONF.api.routes['/profiles'].max) {
         db.collection('test').find({_id:{ $in: accounts }} ).toArray((err, result) => {
           if (err) return console.log(err);
             res.status(200).send(result);
