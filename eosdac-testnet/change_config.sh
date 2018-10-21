505bdf23b4050fe7f3668b57472b346d8d147386
Michael Yeates <michaeljyeates@gmail.com>
2018-10-22 00:58:09 +0200

    Change permission used, cancel existing proposal and create enough signatures to exec the proposal

--- eosdac-testnet/change_config.sh
+++ eosdac-testnet/change_config.sh
@@ -21,10 +21,11 @@ run_cmd() {
 
 ./msig/gen_requested_permissions.py
 
-run_cmd "push action -s -d -j daccustodian updateconfig dac_config2.json -p daccustodian@active > updateconfig.trx"
+run_cmd "push action -s -d -j daccustodian updateconfig dac_config2.json -p dacauthority@high > updateconfig.trx"
+run_cmd "multisig cancel eosdaccustab updconfig eosdaccustab"
 run_cmd "multisig propose_trx updconfig ./requested_perms.json updateconfig.trx eosdaccustab"
 
-for x in {a..i}
+for x in {a..j}
 do
   CUST="eosdaccusta$x"
   run_cmd "multisig approve eosdaccustab updconfig '{\"actor\":\"$CUST\", \"permission\":\"active\"}' -p $CUST"
